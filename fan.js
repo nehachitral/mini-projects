1e2345bad776f09a5315cfa2a8204a8cceef300a
nehachitral <155183549+nehachitral@users.noreply.github.com>
2024-02-23 00:28:30 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,21 @@
+let a=document.getElementById("img");
+function on_function()
+{
+    a.style.animationDuration=3+"s";
+}
+function off_function()
+{
+    a.style.animationDuration=0+"s";
+}
+function on_function_1()
+{
+    a.style.animationDuration=1+"s";
+}
+function on_function_2()
+{
+    a.style.animationDuration=0.5+"s";
+}
+function on_function_3()
+{
+    a.style.animationDuration=0.1+"s";
+}
